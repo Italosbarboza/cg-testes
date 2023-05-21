ed83937013476ae38ffd666df915069224a55a36
Italo Barboza <italo_barboza@hotmail.com>
2023-05-21 14:52:53 -0300

    CG testes

---
+++ README.md
@@ -0,0 +1,24 @@
+# mini-MarioWorld
+A CG Discipline's project. Coded in Python with OpenCV and Numpy.
+
+### Dependencies
+- Python >= 3.11
+  - OpenCV
+  - Numpy
+
+### Installation
+The follow commands must be executed on the project's root directory.
+1. It's strongly recommended to create a [virtualenv](https://docs.python.org/3/library/venv.html) to run this project. To consider the `.gitignore` file, we recommend name it `mini-mario`. To do it, execute:
+    > python -m venv _mini-mario_
+
+2. Now, with [pip](https://pypi.org/project/pip/) package manager, execute to install the dependencies:
+    > pip install -r _requirements.txt_
+### Tests
+Execute the animations algorithms on `tests/` directory with follow the commands:
+>python _<span>animation.py</span>_
+
+>python _<span>animation_with_window.py</span>_
+
+>python _<span>animation_with_multi_window.py</span>_
+
+**Note:** Press the `Esc` key to end.
---
+++ miniCG/poly.py
@@ -0,0 +1,74 @@
+from miniCG import img
+import numpy as np
+
+def create():
+    return np.array([], np.float32)
+
+def insert_dot(pol, dot):
+    return np.array(pol.tolist() + [dot], np.float32)
+
+def intersec(scan, seg):
+    pi = seg[0]
+    pf = seg[1]
+
+    y = scan
+
+    # If it's a horizontal segment, there's no intersection
+    if (pi[1] == pf[1]):
+        return np.array([-1, 0, 0, 0], np.float32)
+    
+    # Swap to guarantee initial dot over
+    if (pi[1] > pf[1]):
+        pi, pf = pf, pi
+
+    # Calculating t
+    t = (y - pi[1])/(pf[1]-pi[1])
+
+    # Calculating x
+    if (t > 0 and t <= 1):
+        x = pi[0] + t*(pf[0]-pi[0])
+        
+        tx = pi[2] + t*(pf[2]-pi[2])
+        ty = pi[3] + t*(pf[3]-pi[3])
+
+        return np.array([x, y, tx, ty], np.float32)
+    
+    # No intersection
+    return np.array([-1, 0, 0, 0], np.float32)
+
+def draw(buf, pol, intst):
+    m = buf
+
+    for i in range(0, pol.shape[0]-1):
+        m = img.strt_line_ddaaa(m, pol[i, 0], pol[i, 1], pol[i+1, 0], pol[i+1, 1], intst)
+
+    m = img.strt_line_ddaaa(m, pol[-1, 0], pol[-1, 1], pol[0, 0], pol[0, 1], intst)
+    
+    return m 
+    
+def draw_circle(center_x, center_y, radius):
+    x = 0
+    y = radius
+    p = 1 - radius
+
+    points = set()
+
+    while x <= y:
+        points.add((x + center_x, y + center_y))
+        points.add((-x + center_x, y + center_y))
+        points.add((x + center_x, -y + center_y))
+        points.add((-x + center_x, -y + center_y))
+        points.add((y + center_x, x + center_y))
+        points.add((-y + center_x, x + center_y))
+        points.add((y + center_x, -x + center_y))
+        points.add((-y + center_x, -x + center_y))
+
+        if p < 0:
+            p += 2 * x + 3
+        else:
+            p += 2 * (x - y) + 5
+            y -= 1
+
+        x += 1
+
+    return points
